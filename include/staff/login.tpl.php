9b73b8d90a3f8321bcf0fdeb8e5dc4f7b25cbf80
Matt Delaney <mcdelaney@gmail.com>
2024-08-21 10:29:32 -0600

    adjust login page css to allow scroll

--- include/staff/login.tpl.php
+++ include/staff/login.tpl.php
@@ -73,11 +73,6 @@ if (($bks=StaffAuthenticationBackend::getExternal())) { ?>
         </div>
     </div>
 </div>
-<div id="poweredBy"><?php echo __('Powered by'); ?>
-    <a href="http://www.osticket.com" target="_blank">
-        <img alt="osTicket" src="images/osticket-grey.png" class="osticket-logo">
-    </a>
-</div>
     <script>
     document.addEventListener('DOMContentLoaded', function() {
         if (undefined === window.getComputedStyle(document.documentElement).backgroundBlendMode) {
